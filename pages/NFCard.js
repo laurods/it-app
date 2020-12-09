57cd1f65a798aec3b6f9e2e30391533f4f167e4b
laurods <itrafor@gmail.com>
2020-12-09 12:22:59 -0200

    Atualizacao 091220c

--- pages/NFCard.js
+++ pages/NFCard.js
@@ -3,85 +3,16 @@ class NFCard extends React.Component {
   constructor() {
     super();
   
-    this.showDetails = this.showDetails.bind(this);
-    this.hideDetails = this.hideDetails.bind(this);
+   
   }
 
-  showDetails(e){
-    const details = e.target.parentNode.lastElementChild;
-    details.style.display = 'block';    
-   }
-   hideDetails(e){
-    const details = e.target.parentNode.lastElementChild;
-    details.style.display = 'none';    
-  }
+  
   
   render() {  
-    const {
-      cean,
-      ceantrib,      
-      xprod,
-      ucom,
-      qcom,
-      picms,
-      perDifcaIcms,
-      vprod,
-      ipi,
-      icmsst,
-      vDifcaIcms,
-      custoTotal,
-      custoUn,
-     } = this.props.nf;
+   
     return (
       <div className="NF-Card">  
-          <tr /*onMouseEnter={this.showDetails} onMouseLeave={this.hideDetails}*/>
-            <td className="xprod">{xprod}</td>
-            <td className="custoUn">{custoUn}</td>
-           
-            <div className="nf-card-details">
-              <div className="nf-card-details-top">
-                <input className="nf-card-details-input-xprod" type='text' value={xprod}></input>
-              </div>
-
-              <div className="nf-card-details-left">
-              <div className="nf-card-details-item">
-                <span> Valor Produtos : {vprod}</span>
-                <br/>
-                <span> Valor IPI: {ipi}</span>
-                <br/>
-                <span>Dif. Icms: {vDifcaIcms}</span>
-                <br/>
-                <span>Subs.Trib: {icmsst}</span>
-                <br/>
-                <span>Custo Total: {custoTotal}</span>
-                <br/>
-                <span>Quant: {qcom} {ucom}</span>
-              </div>
-              </div>
-
-              <div className="nf-card-details-right">
-              <div className="nf-card-details-input">
-                <div className="nf-card-details-input-cx">
-                  Cod. Caixa
-                  <input className="nf-card-details-input-default" type='text' value={cean}></input>
-                  Quant.
-                  <input className="nf-card-details-input-default" type='text' value={qcom}></input>
-                </div>
-                <div className="nf-card-details-input-item">
-                Cod. Item
-                <input className="nf-card-details-input-default"type='text' value={ceantrib}></input>
-                Quant.
-                <input className="nf-card-details-input-default" type='text' value='0'></input>
-                </div>              
-               
-              </div>
-              </div>
-              <div className="nf-card-details-button">
-                <button>Salvar</button>
-              </div>      
-            </div>
-                        
-          </tr>
+          <p>NF Cards</p>
           
              
       </div>
