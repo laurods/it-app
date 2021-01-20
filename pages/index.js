911bc3e82385f3eb15ad604e148e898f7652cf36
laurods <itrafor@gmail.com>
2021-01-20 18:56:52 -0200

    Atualizaçoes 20012021c

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,35 @@
-import React from 'react';
-import Button from '@material-ui/core/Button';
+import React from "react";
+import Container from "@material-ui/core/Container";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Typography from "@material-ui/core/Typography";
 function Home() {
   return (
-    <Button variant="contained" color="primary">
-      Hello World
-    </Button>
+    <div>
+      <CssBaseline />
+      <main>
+        <Container maxWidth="sm">
+          <Typography
+            component="h1"
+            variant="h2"
+            align="center"
+            color="textPrimary"
+            gutterBottom
+          >
+            Lauro de Sá
+          </Typography>
+          <Typography
+            variant="h5"
+            align="center"
+            color="textSecondary"
+            paragraph
+          >
+            Something short and leading about the collection below—its contents,
+            the creator, etc. Make it short and sweet, but not too short so
+            folks don&apos;t simply skip over it entirely.
+          </Typography>
+        </Container>
+      </main>
+    </div>
   );
 }
 
